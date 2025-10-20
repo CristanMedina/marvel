29f8d73fd1e46255d113453b3d1a295bb5de1320
Cristan Medina <crishighlights@gmail.com>
2025-10-20 09:33:26 -0700

    primer commit

---
+++ src/app/components/personaje-detail/personaje-detail.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personaje-detail',
+  templateUrl: './personaje-detail.component.html',
+  styleUrls: ['./personaje-detail.component.scss'],
+})
+export class PersonajeDetailComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ src/app/components/personaje-list/personaje-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personaje-list',
+  templateUrl: './personaje-list.component.html',
+  styleUrls: ['./personaje-list.component.scss'],
+})
+export class PersonajeListComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
